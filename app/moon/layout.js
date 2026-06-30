9966a8a141f01b7ef8c76145be9c5e4bfadd9c59
YORIMITI2698 <contact@yorimiti.jp>
2026-07-01 01:20:46 +0900

    seo: add app/moon/layout.js

---
+++ app/moon/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'YORIMITIについて',
+  description: '「カタチも、視点も、もっと自由に。」YORIMITIが大切にしている想いとコンセプトをご紹介します。',
+  alternates: { canonical: '/moon' },
+  openGraph: {
+    title: 'YORIMITIについて｜YORIMITI',
+    description: '「カタチも、視点も、もっと自由に。」YORIMITIが大切にしている想いとコンセプトをご紹介します。',
+    url: 'https://yorimiti.jp/moon',
+  },
+}
+
+export default function Layout({ children }) {
+  return children
+}
